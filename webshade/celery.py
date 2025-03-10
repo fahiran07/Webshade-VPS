05c958c456ad135093f7a7b184fac99eef42fc46
fahi124432 <fffahiran@gmail.com>
2025-03-10 23:06:37 +0530

    ini

--- webshade/celery.py
+++ webshade/celery.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webshade.settings')
 
 app = Celery('webshade')
