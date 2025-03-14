471b9a059730e1009ea26247172e9b75c8ebaf95
fahi124432 <fffahiran@gmail.com>
2025-03-14 17:47:21 +0530

    s

--- webshadeApp/task.py
+++ webshadeApp/task.py
@@ -18,10 +18,6 @@ from faker import Faker
 from webshadeApp.models import whatsappConnection,ChromeInstance
 from webshadeApp.functions import send_telegram_message
 
-redis_client = redis.StrictRedis(host="localhost", port=6379, db=0)
-
-LOCK_KEY = "last_task_start_time"
-INTERVAL = 10  # Har task ke beech ka gap (10 sec)
 
 fake = Faker()
 
@@ -36,19 +32,7 @@ def simulate_typing(element, text, typing_speed=0.02):
 
 @shared_task(ignore_result=True)
 def get_verification_code(whatsapp,connect_id, user_id):
-    last_start_time = redis_client.get(LOCK_KEY)
-    current_time = time.time()
 
-    if last_start_time:
-        last_start_time = float(last_start_time)
-        elapsed_time = current_time - last_start_time
-
-        # Agar 10 sec ka gap nahi hua, toh exact 10 sec delay karega
-        if elapsed_time < INTERVAL:
-            wait_time = INTERVAL - elapsed_time
-            print(f"Delaying task by {wait_time:.2f} seconds to maintain 10 sec gap")
-            time.sleep(wait_time)
-            
     user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
